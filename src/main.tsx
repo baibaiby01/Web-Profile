2e63c393ad161e6f6f679c1e81ed9e476d541ce0
Web Profile <pitchayaporn.wo@rmuti.ac.th>
2025-07-10 18:04:15 +0700

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Home'; 
 import About from './About';
 import Portfolio from './Portfolio';
